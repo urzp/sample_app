d2ab87b4a38af2e01e7b63b963eb6ac04aa9b5fe
urzp <urzp002@mail.ru>
2016-08-19 07:11:03 +0300

    add defaul rout

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
