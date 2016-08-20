cd7e670c7ebf4f467464abc7384aa849bef868ff
urzp <urzp002@mail.ru>
2016-08-20 15:54:36 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
 
+  match '/signup',  to: 'users#new',            via: 'get'
+
   match '/help',    to: 'static_pages#help',    via: 'get'
 
   match '/about',   to: 'static_pages#about',   via: 'get'
